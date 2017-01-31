597e5eb176c0bac1728805cc673d87f54f18d35c
Natalia Vazquez Hernandez <natalia.e.vazquez.hdz@gmail.com>
2017-01-31 23:46:00 +0000

    shapes

--- script.js
+++ script.js
@@ -12,7 +12,45 @@ right(90);
 forward(50);
 }
 
-// Type your function call below
+
+function triangle(){
+    
+    forward(40);
+    right(80);
+    forward(40);
+    right(140);
+    forward(60);
+     
+}
 
 
+function house(){
+   
+    forward(40);
+    right(60);
+    forward(60);
+    right(60);
+    forward(60);
+    right(60);
+    forward(40);
+    right(90);
+    forward(100);
+    
+}
+function pentagon(){
+    right(60);
+    forward(50);
+    right(40);
+    forward(50);
+    right(60);
+    forward(20);
+    right(80);
+    forward(30);
+    right(45);
+    forward(70);
+    
+}
+
+
+pentagon();
 stamp();
